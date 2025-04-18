29dfedbb138b40eea7b92e38bb5d2b777a201838
chnnick <nickchenschool@gmail.com>
2025-04-18 00:44:11 -0400

    new program bentos + animation + bentos now are used as buttons to redirect to different parts of /programs page

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { ChevronRight } from "lucide-react";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -50,17 +51,19 @@ const BentoCard = ({
     key={name}
     initial={{ opacity: 0, x: initialX, y: initialY }}
     whileInView={{ opacity: 1, x: 0, y: 0 }}
+    whileHover={{ scale: 1.02 }}
     viewport={{ once: true }}
-    transition={{ duration: 0.6, ease: "easeOut" }}
+    transition={{ duration: 0.3, ease: "easeOut" }}
     className={cn(
       "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
       "bg-white",
       "transform-gpu",
+      "cursor-pointer",
       className,
     )}
   >
+    <Link href={href} className="absolute inset-0 z-20" />
     <div>{background}</div>
-    
     <div className="pointer-events-none absolute bottom-0 z-10 flex transform-gpu flex-col gap-1 p-5 transition-all duration-300 group-hover:opacity-0">
       <Icon className="h-10 w-10 origin-left transform-gpu text-neutral-300 ease-in-out" />
       <h3 className="text-lg font-semibold text-neutral-300">
@@ -78,12 +81,10 @@ const BentoCard = ({
       
       <div className="mt-2 flex flex-col transform-gpu translate-y-10 opacity-0 transition-all delay-100 duration-300 group-hover:translate-y-0 group-hover:opacity-100">
         <p className="max-w-lg text-sm text-neutral-300">{description}</p>
-        <Button variant="ghost" asChild size="sm" className="pointer-events-auto mt-2 text-neutral-300">
-          <a href={href}>
-            {cta}
-            <ChevronRight className="ml-2 h-4 w-4" />
-          </a>
-        </Button>
+        <div className="mt-2 flex items-center text-neutral-300">
+          {cta}
+          <ChevronRight className="ml-2 h-4 w-4 transition-transform group-hover:translate-x-1" />
+        </div>
       </div>
     </div>
     
